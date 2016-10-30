9d2a8a14432181bd27c473aca3d7210e9b083547
kasum <kasum@DESKTOP-7OS1N5Q>
2016-10-30 14:37:54 +0900

    add disk detect

--- AutoUSBBackup/MainForm.Designer.cs
+++ AutoUSBBackup/MainForm.Designer.cs
@@ -29,22 +29,35 @@
         private void InitializeComponent()
         {
             this.lbMessage = new System.Windows.Forms.Label();
+            this.driveList = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // lbMessage
             // 
             this.lbMessage.AutoSize = true;
+            this.lbMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lbMessage.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lbMessage.Location = new System.Drawing.Point(95, 106);
+            this.lbMessage.Location = new System.Drawing.Point(12, 9);
+            this.lbMessage.MinimumSize = new System.Drawing.Size(100, 100);
             this.lbMessage.Name = "lbMessage";
-            this.lbMessage.Size = new System.Drawing.Size(0, 27);
+            this.lbMessage.Size = new System.Drawing.Size(100, 100);
             this.lbMessage.TabIndex = 0;
+            // 
+            // driveList
+            // 
+            this.driveList.FormattingEnabled = true;
+            this.driveList.ItemHeight = 12;
+            this.driveList.Location = new System.Drawing.Point(118, 9);
+            this.driveList.Name = "driveList";
+            this.driveList.Size = new System.Drawing.Size(165, 172);
+            this.driveList.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(292, 190);
+            this.Controls.Add(this.driveList);
             this.Controls.Add(this.lbMessage);
             this.Name = "MainForm";
             this.Text = "MainForm";
@@ -57,5 +70,6 @@
         #endregion
 
         private System.Windows.Forms.Label lbMessage;
+        private System.Windows.Forms.ListBox driveList;
     }
 }
--- AutoUSBBackup/MainForm.cs
+++ AutoUSBBackup/MainForm.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace AutoUSBBackup
 {
     public partial class MainForm : Form
     {
+        private IEnumerable<DriveInfo> _drives;
         UInt32 WM_DEVICECHANGE = 0x0219;
         UInt32 DBT_DEVTUP_VOLUME = 0x02;
         UInt32 DBT_DEVICEARRIVAL = 0x8000;
@@ -20,16 +22,39 @@ namespace AutoUSBBackup
         public MainForm()
         {
             InitializeComponent();
+            refreshDrives();
         }
 
+        public void refreshDrives()
+        {
+            _drives = DriveInfo.GetDrives().Where(drive => drive.IsReady && drive.DriveType == DriveType.Removable);
 
+            driveList.Items.Clear();
+            
+            foreach(DriveInfo info in _drives)
+            {
+                String name = info.Name;
+                DirectoryInfo dir = info.RootDirectory;
+                String size = info.TotalSize.ToString();
+
+                driveList.Items.Add(name + "/" + dir.ToString() + "/" + size);
+            }
+        }
+        
         protected override void WndProc(ref Message m)
         {
+            // USB device attach event
             if ((m.Msg == WM_DEVICECHANGE) && (m.WParam.ToInt32() == DBT_DEVICEARRIVAL))
             {
-                lbMessage.Text = "헐!";
+                refreshDrives();
             }
-            
+
+            // USB device detach event
+            if ((m.Msg == WM_DEVICECHANGE) && (m.WParam.ToInt32() == DBT_DEVICEARRIVAL))
+            {
+                refreshDrives();
+            }
+
             base.WndProc(ref m);
         }
 
