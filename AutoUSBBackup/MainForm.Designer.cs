2ff8e40791e35375e164528c8825d34894dc9b72
kasum <kasum@DESKTOP-7OS1N5Q>
2016-10-30 14:19:35 +0900

    Base Commit

---
+++ AutoUSBBackup/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AutoUSBBackup
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.AutoSize = true;
+            this.lbMessage.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbMessage.Location = new System.Drawing.Point(95, 106);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(0, 27);
+            this.lbMessage.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lbMessage);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMessage;
+    }
+}
---
+++ AutoUSBBackup/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoUSBBackup
+{
+    public partial class MainForm : Form
+    {
+        UInt32 WM_DEVICECHANGE = 0x0219;
+        UInt32 DBT_DEVTUP_VOLUME = 0x02;
+        UInt32 DBT_DEVICEARRIVAL = 0x8000;
+        UInt32 DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            if ((m.Msg == WM_DEVICECHANGE) && (m.WParam.ToInt32() == DBT_DEVICEARRIVAL))
+            {
+                lbMessage.Text = "헐!";
+            }
+            
+            base.WndProc(ref m);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
